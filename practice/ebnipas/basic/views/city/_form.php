eac684c9462812f333cd8a06642c690fe11fdf15
Eduardo B. Nipas Jr <EdNipas@gmail.com>
2015-02-21 18:36:00 +0800

    Added dropDownList to city/_form.php Added dropDownList to province/_form.php

--- practice/ebnipas/basic/views/city/_form.php
+++ practice/ebnipas/basic/views/city/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use app\models\province;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\city */
@@ -19,7 +21,18 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'city_description')->textInput(['maxlength' => 45]) ?>
 
     <?= $form->field($model, 'province_id')->textInput() ?>
-
+	<?php
+	$province=province::find()->all();
+	//use yii\helpers\ArrayHelper;
+	$listData=ArrayHelper::map($province,'id','province_description');
+	
+	echo $form->field($model,'province')->dropDownList(
+										$listData,
+										['prompt'=>'Select..']);
+						
+	?>
+	
+	
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
