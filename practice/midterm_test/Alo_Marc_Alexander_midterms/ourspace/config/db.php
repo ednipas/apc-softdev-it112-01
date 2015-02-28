da2c7c8eef43aa894ce678b070f2891391c11d58
Marc Alo <APCmgalo48@gmail.com>
2015-02-28 09:17:24 +0800

    Did Step E and F refer to practice/midterm_test/Midterm_Handson_Readme.txt

--- practice/midterm_test/Alo_Marc_Alexander_midterms/ourspace/config/db.php
+++ practice/midterm_test/Alo_Marc_Alexander_midterms/ourspace/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=marktroi',
+    'dsn' => 'mysql:host=localhost;dbname=softdev_alo_marc_ourspace',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
