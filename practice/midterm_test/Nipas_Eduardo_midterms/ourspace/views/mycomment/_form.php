7b6da6d47405ee1c8b40c51295a6f22b6ab768e8
Eduardo B. Nipas Jr <EdNipas@gmail.com>
2015-03-01 19:24:57 +0800

    Add dropDownList in my comment myaddress_id as lastname

--- practice/midterm_test/Nipas_Eduardo_midterms/ourspace/views/mycomment/_form.php
+++ practice/midterm_test/Nipas_Eduardo_midterms/ourspace/views/mycomment/_form.php
@@ -2,28 +2,45 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use app\models\myaddress;
+use yii\helpers\ArrayHelper;
+
 
 /* @var $this yii\web\View */
-/* @var $model app\models\myaddress */
+/* @var $model app\models\mycomment */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="myaddress-form">
+<div class="mycomment-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'id')->textInput() ?>
 
-    <?= $form->field($model, 'firstname')->textInput(['maxlength' => 30]) ?>
+ 
+	
+		<?php
+		$myaddress=myaddress::find()->all();
+		//use yii\helpers\ArrayHelper;
+		$listData=ArrayHelper::map($myaddress,'id','lastname');
 
-    <?= $form->field($model, 'middlename')->textInput(['maxlength' => 30]) ?>
+		echo $form->field($model,'myaddress_id')->dropDownList(
+											$listData,
+											['prompt'=>'Select..']);
 
-    <?= $form->field($model, 'lastname')->textInput(['maxlength' => 30]) ?>
+?>
 
-    <?= $form->field($model, 'gender')->textInput(['maxlength' => 1]) ?>
+    <?= $form->field($model, 'author')->textInput(['maxlength' => 255]) ?>
+
+    <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
 
     <?= $form->field($model, 'created_at')->textInput() ?>
+	
 
+
+
+
+	
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
