7e8b9651d8747d8a4c217737a9939d3ae8e1d4a7
Dan Jhon Paul Y. Gahallon <dygahallon@gmail.com>
2015-03-02 00:34:56 +0800

    Update search field of myaddress: models/MycommentSearch.php

--- practice/midterm_test/Gahallon_DanJhonPaul_midterms/ourspace/models/MycommentSearch.php
+++ practice/midterm_test/Gahallon_DanJhonPaul_midterms/ourspace/models/MycommentSearch.php
@@ -18,8 +18,8 @@ class MycommentSearch extends Mycomment
     public function rules()
     {
         return [
-            [['id', 'myaddress_id'], 'integer'],
-            [['author', 'body', 'created_at'], 'safe'],
+             [['id'], 'integer'],
+            [['author', 'body', 'created_at','myaddress_id'], 'safe'],
         ];
     }
 
