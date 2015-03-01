cd74093bfb9aab5834d267d8bba12195a2f78e47
Dan Jhon Paul Y. Gahallon <dygahallon@gmail.com>
2015-03-01 11:01:34 +0800

    Modified File practice/midterm_test/Gahallon_DanJhonPaul_midterms/ourspace/config/db.php

--- practice/midterm_test/Gahallon_DanJhonPaul_midterms/ourspace/config/db.php
+++ practice/midterm_test/Gahallon_DanJhonPaul_midterms/ourspace/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=softdev_gahallon_danjhonpaul_ourspace',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
