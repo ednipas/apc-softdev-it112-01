e2ff326d12688ca54faee3a1c49b733927afcd58
Dan Jhon Paul Y. Gahallon <dygahallon@gmail.com>
2015-03-15 03:53:48 +0800

    Created a model and crud for Order Table

---
+++ trunk/Application/marktroibags3.0/backend/models/OrderSearch.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use backend\models\Order;
+
+/**
+ * OrderSearch represents the model behind the search form about `backend\models\Order`.
+ */
+class OrderSearch extends Order
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'product_id', 'promotion_id', 'id'], 'integer'],
+            [['date', 'status'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Order::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'user_id' => $this->user_id,
+            'product_id' => $this->product_id,
+            'promotion_id' => $this->promotion_id,
+            'id' => $this->id,
+        ]);
+
+        $query->andFilterWhere(['like', 'date', $this->date])
+            ->andFilterWhere(['like', 'status', $this->status]);
+
+        return $dataProvider;
+    }
+}
