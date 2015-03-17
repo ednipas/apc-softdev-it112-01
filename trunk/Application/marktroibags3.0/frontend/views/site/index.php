e1349bdb52e00ab009d6be5dfd2ec4b9eaf89ce9
Dan Jhon Paul Y. Gahallon <dygahallon@gmail.com>
2015-03-17 11:28:00 +0800

    Modified marktroibags3.0/frontend/views/site/index.php

--- trunk/Application/marktroibags3.0/frontend/views/site/index.php
+++ trunk/Application/marktroibags3.0/frontend/views/site/index.php
@@ -5,7 +5,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>MarkTroi Bags Interactive website</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
