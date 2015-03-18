c70e97b22100e3049b5d95949a87b7cd2ab7ff99
Eduardo B. Nipas Jr <EdNipas@gmail.com>
2015-03-18 19:15:40 +0800

    Revert "Creadted crcuD for Product Table"
    This reverts commit a1842937bb3c1558b767acd55bc166bfe206b38f.

--- trunk/Application/marktroibags3.0/frontend/views/layouts/main.php
+++ trunk/Application/marktroibags3.0/frontend/views/layouts/main.php
@@ -34,7 +34,7 @@ AppAsset::register($this);
             ]);
             $menuItems = [
                 ['label' => 'Home', 'url' => ['/site/index']],
-                ['label' => 'Product', 'url' => ['/models/Product']],
+                ['label' => 'Product', 'url' => ['/site/contact']],
                 ['label' => 'About', 'url' => ['/site/about']],
                 ['label' => 'Contact', 'url' => ['/site/contact']],
                 
